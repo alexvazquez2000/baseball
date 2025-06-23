64b95c3f7fea8278f85c8cf98a2f5eeec617751c
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-06-23 10:26:56 -0600

    read secret locally vs on cloud

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # config.py
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_MYSQL')
+    #
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_MYSQL')
+    if SQLALCHEMY_DATABASE_URI :
+      #print("found DB setting")
+      pass
+    else :
+      #print("did not found setting")
+      load_dotenv()
+      SQLALCHEMY_DATABASE_URI = os.getenv('DB_MYSQL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #https://flask-sqlalchemy.readthedocs.io/en/stable/config/
     SQLALCHEMY_ENGINE_OPTIONS = { 'pool_recycle': 280, 'pool_pre_ping': True }
