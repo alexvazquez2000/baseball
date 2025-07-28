a2afaa31010d573acf5358286f9164668a0fe3e3
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-07-28 16:32:22 -0600

    Debugging the performance on a render_template
    Players and parents pages have about a 5 sec performance due to jinja2
    having to process all the rows.  Consider using pagination
    https://www.digitalocean.com/community/tutorials/how-to-query-tables-and-paginate-data-in-flask-sqlalchemy#step-2-querying-records

--- play_ball/players/players_bp.py
+++ play_ball/players/players_bp.py
@@ -13,13 +13,33 @@ players_bp = Blueprint('players', __name__, template_folder='templates')
 @players_bp.route('/')
 #@login_required
 def list_players():
+    #now = datetime.now()
+    #formatted_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    #print(f"start {formatted_time}")
+
     players = Players.query.all()
+
+    #now = datetime.now()
+    #formatted_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    #print(f"got player {formatted_time}")
+
     (current_season_id, current_season_name) = get_current_season()
+
+    #now = datetime.now()
+    #formatted_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    #print(f"got season {formatted_time}")
+    
     #players = db.session.query(Players, teams_players, func.count(teams_players.teams_id))
     #players = db.session.query(Players, teams_players).outerjoin(Players.id==teams_players.players_id ).all
     #.outerjoin(Teams, Teams.id==teams_players.teams_id).filter(Teams.season_id==current_season_id).all()
-    return render_template('players.html', players=players)
-
+    #return render_template('players.html', players=players)
+    rh = render_template('players.html', players=players)
+    
+    #now = datetime.now()
+    #formatted_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    #print(f"render {formatted_time}")
+    
+    return rh
 @players_bp.route('/player', methods=['GET', 'POST'])
 #@login_required
 def edit_player():
