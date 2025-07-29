f91eae899976c0938d08da3399bc52def5c36e06
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-07-29 17:44:22 -0600

    Fix login page url

--- play_ball/auth/auth_bp.py
+++ play_ball/auth/auth_bp.py
@@ -45,7 +45,7 @@ def register():
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
         #TODO: instead take them to a page to enter parent and player information
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login_page'))
     return render_template('register.html', title='Register', form=form)
 
 @auth_bp.route('/logout')
